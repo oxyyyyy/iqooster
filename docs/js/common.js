37153a0404556f55ddfc626ee09dad3bc5dcc5f4
Alexandr Vlasenko <olexandr.vlasenko@gmail.com>
2018-01-24 19:55:34 +0200

    docs for github

---
+++ docs/js/common.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+	
+	// media query event handler
+	if (matchMedia) {
+		const sm = window.matchMedia("(max-width: 991px)");
+		sm.addListener(WidthChange);
+		WidthChange(sm);
+	}
+
+	// media query change
+	function WidthChange(sm) {
+		if (sm.matches) {
+			$("#course_carousel").glide({
+				type: "carousel"
+			});
+			$("#benefits_carousel").glide({
+				type: "carousel"
+			});
+			$("#consist_carousel").glide({
+				type: "carousel"
+			});
+		} else {
+			$("#course_carousel").glide().data('glide_api').destroy();
+			$("#benefits_carousel").glide().data('glide_api').destroy();
+			$("#consist_carousel").glide().data('glide_api').destroy();
+		}
+	}
+
+});
